def1a9b6dc4e839d8b37f7bdfbe9b324ca3e8809
janokular <janekokularczyk@gmail.com>
2025-05-13 16:59:18 +0200

    staged changes

--- task/services/id_checker.py
+++ task/services/id_checker.py
@@ -1,3 +1,21 @@
-def is_in_range(id, task_list):
+import json
+
+
+def is_in_range(id, file):
     '''Check if task id is in range'''
-    return True
+    ids = []
+
+    with open(file) as tasks:
+        tasks = json.load(tasks)
+
+        for task in tasks['tasks']:
+            ids.append(task.get('id'))
+
+    if id <= 0:
+        print('(ID: {}) cannot be lower than 1'.format(id))
+        return False
+    elif id > ids[-1]:
+        print('(ID: {}) is out of range, max value (ID: {})'.format(id, ids[-1]))
+        return False
+    else:
+        return id in ids
