b176a27bb4b332301663161ba044782a543de924
janokular <janekokularczyk@gmail.com>
2025-05-15 14:12:11 +0200

    staged changes

---
+++ utils/pretty_print.py
@@ -0,0 +1,38 @@
+rows = []
+maxes = {'id_len': 0, 'desc_len': 0, 'status_len': 0}
+
+
+def add_row(task):
+    rows.append(task)
+
+    if maxes['id_len'] < len(str(task['id'])):
+        maxes['id_len'] = len(str(task['id']))
+
+    if maxes['desc_len'] < len(task['description']):
+        maxes['desc_len'] = len(task['description'])
+
+    if maxes['status_len'] < len(task['status']):
+        maxes['status_len'] = len(task['status'])
+
+
+def display_table():
+    max_id_len = maxes['id_len']
+    max_desc_len = maxes['desc_len']
+    max_status_len = maxes['status_len']
+
+    if rows:
+        print('·-' + '-' * max_id_len + '-' 
+            + '·-' + '-' * max_desc_len + '-' 
+            + '·-' + '-' * max_status_len 
+            + '-·--------------------------·--------------------------·')
+
+        for row in rows:
+            print('| {} '.format(row['id']) + ' ' * (max_id_len - len(str(row['id'])))
+                    + '| {} '.format(row['description']) + ' ' * (max_desc_len - len(row['description']))
+                    + '| {} '.format(row['status']) + ' ' * (max_status_len - len(row['status']))
+                    + '| {} | {} |'.format(row['createdAt'], row['updatedAt']))
+
+        print('·-' + '-' * max_id_len + '-' 
+            + '·-' + '-' * max_desc_len + '-' 
+            + '·-' + '-' * max_status_len 
+            + '-·--------------------------·--------------------------·')
