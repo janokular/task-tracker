4ff1c5f9cc17aae0e2f55be4a05dbd9839268fad
janokular <janekokularczyk@gmail.com>
2025-05-28 20:28:11 +0200

    staged changes

--- utils/parser.py
+++ utils/parser.py
@@ -7,23 +7,23 @@ def parse_arguments():
 
     subparsers = parser.add_subparsers(dest='action')
 
-    add_parser = subparsers.add_parser('add', help='Add a new task')
+    add_parser = subparsers.add_parser('add', help='add a new task')
     add_parser.add_argument('task_description')
 
-    update_parser = subparsers.add_parser('update', help='Update a task')
+    update_parser = subparsers.add_parser('update', help='update a task')
     update_parser.add_argument('task_id', type=int)
     update_parser.add_argument('task_description')
 
-    delete_parser = subparsers.add_parser('delete', help='Delete a task')
+    delete_parser = subparsers.add_parser('delete', help='delete a task')
     delete_parser.add_argument('task_id', type=int)
 
-    mark_in_progress_parser = subparsers.add_parser('mark-in-progress', help='Mark task as in progress')
+    mark_in_progress_parser = subparsers.add_parser('mark-in-progress', help='mark task as in progress')
     mark_in_progress_parser.add_argument('task_id', type=int)
 
-    mark_done_parser = subparsers.add_parser('mark-done', help='Mark task as done')
+    mark_done_parser = subparsers.add_parser('mark-done', help='mark task as done')
     mark_done_parser.add_argument('task_id', type=int)
 
-    list_parser = subparsers.add_parser('list', help='List tasks')
+    list_parser = subparsers.add_parser('list', help='list tasks')
     list_parser.add_argument('task_status', nargs='?', choices=['done', 'todo', 'in-progress'])
 
     args = parse_arguments()
