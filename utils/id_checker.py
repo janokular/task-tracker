9c8050a7444847f98673c34667c407cac316c0ed
janokular <janekokularczyk@gmail.com>
2025-07-14 23:20:32 +0200

    updated id_checker.py

--- utils/id_checker.py
+++ utils/id_checker.py
@@ -3,19 +3,12 @@ import json
 
 def is_in_range(id, file):
     '''Check if task id is in range'''
-    ids = []
-
     with open(file) as tasks:
         tasks = json.load(tasks)
 
         for task in tasks['tasks']:
-            ids.append(task['id'])
+            if id == task['id']
+                return True
 
-    if not ids:
-        raise Exception('No tasks to delete')
-    elif id < 1:
-        raise Exception(f'(ID: {id}) cannot be lower than 1')
-    elif id > ids[-1]:
-        raise Exception(f'(ID: {id}) is out of range, max value (ID: {ids[-1]})')
-    else:
-        return id in ids
+        print(f'(ID: {id}) is out of range')
+        return False
