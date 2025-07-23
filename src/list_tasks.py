20e94b95b243d0815c48817b6dc127ed1b48ae60
Jan <janekokularczyk@gmail.com>
2025-07-23 16:15:26 +0200

    updated list_tasks.py

--- src/list_tasks.py
+++ src/list_tasks.py
@@ -7,15 +7,10 @@ def list(status: str, file):
     tasks_for_listing = []
 
     with open(file) as tasks_json:
-        # Load data form tasks.json
         tasks = json.load(tasks_json)
         
-        # Loop thorught tasks
         for task in tasks['tasks']:
-            if status == None:
-                tasks_for_listing.append(task)
-            # Filter lisiting based on task status
-            elif status == task.get('status'):
+            if status == None or status == task.get('status'):
                 tasks_for_listing.append(task)
     
     if tasks_for_listing:
